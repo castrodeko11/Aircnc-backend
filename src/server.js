1ef3f9f02c7743f4f30d058eb9396110c7ddc618
André Oliveira <castrodeko11@hotmail.com>
2020-01-26 11:25:13 -0300

    Configura porta do Heroku

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use('/files',express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
 
 //req.query = Acessar query params (para filtros)
 //req.params = Acessar route params (para edição, delete)
