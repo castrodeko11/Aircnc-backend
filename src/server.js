486c5222979739401cb9c89b3765eeeb05e368f6
André Castro de Oliveira <32251619+castrodeko11@users.noreply.github.com>
2022-09-19 20:05:47 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -42,7 +42,7 @@ io.on('connection', socket => {
 // app.use(cors({origin:'http://localhost:3333'}));
 app.use((req, res, next) => {
     req.io = io;
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", process.env.API_URL);
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "*");
     req.connectedUsers = connectedUsers;
