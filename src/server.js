c56de7918266d10f0b1f6e4187038b8f1c49509c
André Castro de Oliveira <32251619+castrodeko11@users.noreply.github.com>
2022-09-19 20:03:34 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -44,6 +44,7 @@ app.use((req, res, next) => {
     req.io = io;
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "*");
     req.connectedUsers = connectedUsers;
 
     return next();
