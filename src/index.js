06d582d0aa69400290c2ba6c84838ba970469fd0
André Castro de Oliveira <castrodeko11@hotmail.com>
2022-09-20 21:23:02 -0300

    Deploy vercel and upload folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./src/routes');
+const routes = require('./routes');
 const cors = require('cors');
 const path = require('path');
 const socketio = require ('socket.io');
