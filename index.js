ee2bfa84c5016da611b0f568772ea0be2d3f1938
André Castro de Oliveira <castrodeko11@hotmail.com>
2022-09-20 21:02:52 -0300

    Change file to index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes');
+const routes = require('./src/routes');
 const cors = require('cors');
 const path = require('path');
 const socketio = require ('socket.io');
@@ -51,12 +51,18 @@ app.use((req, res, next) => {
 
 })
 
+app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳')
+  })
+
 app.use(cors());
 app.use(express.json());
 app.use('/files',express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
 server.listen(process.env.PORT || 3333);
+
+module.exports = app
 
 //req.query = Acessar query params (para filtros)
 //req.params = Acessar route params (para edição, delete)
